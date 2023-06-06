4a2ae3f233d6441488297520d2cbbc299a4ba04b
Leandro Arturi <lea.arturi@gmail.com>
2023-06-05 22:23:36 -0300

    Sin Slug

--- pages/smarthome/index.tsx
+++ pages/smarthome/index.tsx
@@ -7,16 +7,11 @@ interface LandingTypes {
 const Home: NextPage<LandingTypes> = ({ slug }) => {
    return (
       <div style={{ paddingLeft: '3rem' }}>
-         <h1>Smarthome</h1>
+         <h1>Smarthome (sin slug)</h1>
          <hr />
          <h3>Slug: {slug}</h3>
       </div>
    );
-};
-
-export const getStaticPaths = async () => {
-   const paths = [{ params: { slug: 'smarthome' } }];
-   return { paths, fallback: 'blocking' };
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
